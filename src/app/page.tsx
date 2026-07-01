c3a03a3c4c77a45bf45a46490e2d9293611e9f1a
Prvy Dev <contact@blackstimeconsulting.com>
2026-07-01 15:36:38 +0100

    Update hero background to Cover 1 (suit/cigar/whiskey)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -147,7 +147,7 @@ export default function LandingPage() {
         {/* Hero background image */}
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="/make_it_a_dark_red_202606272046.jpeg"
+          src="/Cover 1.jpg"
           alt=""
           aria-hidden
           className="absolute inset-0 h-full w-full object-cover"
