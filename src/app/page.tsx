f78b27fd86f90cc993524580aee703786da0b931
Prvy Dev <contact@blackstimeconsulting.com>
2026-07-01 15:41:16 +0100

    Revert hero background to original bar/lounge image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -147,7 +147,7 @@ export default function LandingPage() {
         {/* Hero background image */}
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="/Cover 4.jpg"
+          src="/make_it_a_dark_red_202606272046.jpeg"
           alt=""
           aria-hidden
           className="absolute inset-0 h-full w-full object-cover"
